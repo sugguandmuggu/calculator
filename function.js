f9230a830121fffabdd2ab25594141a0cac45e02
Saksham <jaiswalsaksham32@gmail.com>
2022-09-02 23:21:30 +0530

    add AC and backspace event listeners

--- function.js
+++ function.js
@@ -2,6 +2,8 @@ const numBtns = document.querySelectorAll(".numBtn");
 const currentScreen = document.querySelector(".currentScreen");
 const lastScreen = document.querySelector(".lastScreen");
 const btns = document.querySelectorAll(".btn");
+const clearBtn = document.querySelector(".clearBtn");
+const backspaceBtn = document.querySelector(".backspaceBtn");
 
 let num1="";
 let num2="";
@@ -12,6 +14,8 @@ let result = 0;
 let resultFlag=0;
 let strike=0;
 let strikeFlag=0;
+let temp1= "";
+let temp2= "";
 
 
 numBtns.forEach((numBtn) => numBtn.addEventListener('click', () => {
@@ -28,11 +32,12 @@ numBtns.forEach((numBtn) => numBtn.addEventListener('click', () => {
     else{
         num2=`${num2}${numBtn.textContent}`;
         currentScreen.textContent=`${currentScreen.textContent}${numBtn.textContent}`;
+        operatorFlag=1;
     }
 }));
 
 btns.forEach((btn) => btn.addEventListener("click", () => {
-    if(btn.id=="equalsBtn")
+    if(btn.id=="equalsBtn" && num1 !== "" && num2 !== "")
     {
         //console.log("hello");
         console.log(num1);
@@ -57,17 +62,69 @@ btns.forEach((btn) => btn.addEventListener("click", () => {
     }
     else if(operatorFlag==1){
         currentScreen.textContent=`${currentScreen.textContent}${btn.textContent}`;
+        temp1=num1;
+        temp2=num2;
         num1=`${operate(operator, +num1, +num2)}`;
         num2="";
         console.log(num1);
         operator=btn.textContent;
+        operatorFlag=3;
     }
-    else{
-        operator=btn.textContent;
-        currentScreen.textContent = `${currentScreen.textContent}${operator}`;
-        operatorFlag=1;
+    else {
+        if(operatorFlag != 3 && num1 !== ""){
+            operator=btn.textContent;
+            currentScreen.textContent = `${currentScreen.textContent}${operator}`;
+            operatorFlag=3;
+        }
+        
     }
-}))
+}));
+
+clearBtn.addEventListener("click", () => {
+    num1 = "";
+    num2 = "";
+    operatorFlag=0;
+    result = 0;
+    resultFlag=0;
+    currentScreen.textContent="";
+    lastScreen.textContent="";
+});
+
+backspaceBtn.addEventListener("click", () => {
+    if( operatorFlag===0 || operatorFlag===2 ){
+        num1 = num1.substring(0, num1.length-1);
+        currentScreen.textContent=num1;
+    }
+    else if(operatorFlag==1){
+        num2 = num2.substring(0, num2.length-1);
+        //console.log('op1');
+        let temp = `${currentScreen.textContent}`;
+        //console.log(temp.slice(0,length-1));
+        currentScreen.textContent = temp.slice(0,length-1);
+        if(num2 === "")
+        {
+            operatorFlag=3;
+        }
+    }
+    else if(operatorFlag===3){
+        //console.log('op3');
+        operator="";
+
+        let temp = `${currentScreen.textContent}`;
+        currentScreen.textContent = temp.slice(0, length-1);
+        if(temp2==="")
+        {
+            operatorFlag=0;
+        }
+        else{
+            num1=temp1;
+            num2=temp2;
+            operatorFlag=1;
+            temp1="";
+            temp2="";
+        }
+    }
+});
 
 
 const add = (a,b)=> {
